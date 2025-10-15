67f981f3e2762f17ca584328de01c5ffef130204
Pedro Henrique <phcasttro@outlook.com>
2025-10-15 12:43:16 -0300

    Corrigindo imports

--- src/main/java/com/phc/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/phc/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,7 @@
 package com.phc.usuario.infrastructure.repository;
 
-import com.phc.aprendendospring.infrastructure.entity.Endereco;
+
+import com.phc.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
