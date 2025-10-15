f495b57fb9d1f7463d2b3ae4194af0a2f8c27f48
Pedro Henrique <phcasttro@outlook.com>
2025-10-15 12:38:41 -0300

    adicionadas classes de security

---
+++ src/main/java/com/phc/usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,22 @@
+package com.phc.usuario.infrastructure.repository;
+
+
+import com.phc.usuario.infrastructure.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    void deleteByEmail(String email);
+}
